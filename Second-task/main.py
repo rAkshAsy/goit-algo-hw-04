4a8007162139e2396c343a00f350c029f16ddf0c
Vladislav Hnyloskurenko <v.hniloskurenko@gmail.com>
2025-06-26 17:28:04 +0300

    added checks and some exception handling

--- Second-task/main.py
+++ Second-task/main.py
@@ -7,36 +7,47 @@ def get_cats_info(path:Path) -> list[dict]:
     '''
     cats_list = [] # List for prepared data
     
-    # Reading and fill list 'cats_list' data from file 
     try:
+        # Reading and fill list 'cats_list' data from file 
         with open(path, 'r', encoding='UTF-8') as file:
             raw_data = file.readlines()
+
+        # Data processing and tuple  return
+        if raw_data:
             for el in raw_data:
-                cat_id, cat_name, cat_age = el.split(',')
-                cats_list.append({
-                    'id': cat_id,
-                    'name': cat_name,
-                    'age': int(cat_age)
-                })
-            return cats_list
-        
+                        if not el.strip():
+                            continue
+                        el = el.strip()
+                        cat = el.split(',')
+                        if len(cat) == 3:
+                            try:
+                                cats_list.append({'id': cat[0], 'name': cat[1], 'age': int(cat[2])})
+                            except ValueError:
+                                cats_list.append('YOU HAVE TROBLE WITH AGE OF THIS CAT')
+
+                        else:
+                            cats_list.append('Something wrong with this cat...')
+
     except FileNotFoundError:
         return 'File not found'
     except OSError:
         return 'File is corrupted'
+    
+    return cats_list
+        
+        
 
+if __name__ == "__main__":
+    # Script that use function total_salary(path)
+    file_name = 'cats.txt'
+    directory = Path(__file__).parent
 
+    cats_info = get_cats_info(directory / file_name)
 
-# Script that use function total_salary(path)
-file_name = 'cats.txt'
-directory = Path(__file__).parent
-
-cats_info = get_cats_info(directory / file_name)
-
-if type(cats_info) == list:
-    for cat in cats_info:
-        print(cat)
-if type(cats_info) == str:
-    print(f'[Error]: {cats_info}')
+    if type(cats_info) == list:
+        for cat in cats_info:
+            print(cat)
+    if type(cats_info) == str:
+        print(f'[Error]: {cats_info}')
 
 
